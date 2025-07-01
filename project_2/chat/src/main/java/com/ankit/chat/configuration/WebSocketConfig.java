454f95c76e013da6f1b4561bb252b040d5295692
Ankit Kaurav <ankitkaurav832@gmail.com>
2025-07-01 16:52:12 +0530

    New project chat app initilized

---
+++ project_2/chat/src/main/java/com/ankit/chat/configuration/WebSocketConfig.java
@@ -0,0 +1,10 @@
+package com.ankit.chat.configuration;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfig {
+}
